4f3c9b28270f779e5ed74650273c8d9b51726f48
tommoir2004 <thomasam04@outlook.com>
2024-09-24 17:44:45 +0100

    created a enum and a struct, writes the name, age rating and release year of a film

--- Program.cs
+++ Program.cs
@@ -4,12 +4,34 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            Console.ReadKey();
+            
+            Movie deadpool3;
+            deadpool3.title = "Deadpool & Wolverine";
+            deadpool3.releaseYear = 2024;
+            deadpool3.price = 19.99;
+            deadpool3.rating = MovieRating._15;
 
-            float someFloat = 3.14159f;
-            Console.WriteLine(someFloat.ToString("0.000"));
+            Console.WriteLine(deadpool3.title + " (" + deadpool3.releaseYear + ") Rated " + deadpool3.rating);
+
             Console.ReadKey();
+        }
+
+        public enum MovieRating
+        {
+            _u,
+            _12A,
+            _12,
+            _15,
+            _18
+        }
+
+        public struct Movie
+        {
+            public string title;
+            public int releaseYear;
+            public double price;
+            public MovieRating rating;
+
         }
     }
 }
